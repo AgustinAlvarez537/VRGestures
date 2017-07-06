276f28267c54defac924c8208c395286924d17b7
Nitzer <agustinalvarez537@gmail.com>
2017-07-05 22:48:32 -0300

    Error corregido en script editor NearSphere

--- Editor/Condition_NearSphereEditor.cs
+++ Editor/Condition_NearSphereEditor.cs
@@ -16,34 +16,34 @@ namespace CardboardGestures.Conditions
 
             myScript.range = EditorGUILayout.FloatField("Rango", myScript.range);
 
-			myScript.showZonaEsfera = EditorGUILayout.Toggle("Mostrar zonaEsfera (Rango)", myScript.showZonaEsfera);
+			myScript.showZonaEsferica = EditorGUILayout.Toggle("Mostrar zona esferica (Rango)", myScript.showZonaEsferica);
 
-            if (myScript.showZonaEsfera)
+            if (myScript.showZonaEsferica)
             {
-                if (myScript.objeto2 != null && myScript.zonaEsfera == null)
+                if (myScript.objeto2 != null && myScript.zonaEsferica == null)
                 {
-                    myScript.zonaEsfera = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                    myScript.zonaEsfera.name = "Range sphere";
-                    myScript.zonaEsfera.transform.position = myScript.objeto2.transform.position;
+                    myScript.zonaEsferica = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                    myScript.zonaEsferica.name = "Range sphere";
+                    myScript.zonaEsferica.transform.position = myScript.objeto2.transform.position;
                     
-                    myScript.zonaEsfera.transform.localScale = new Vector3(myScript.range * 2, myScript.range * 2, myScript.range * 2);
-                    //myScript.zonaEsfera.GetComponent<Renderer>().material.shader = Shader.Find("Unlit/Transparent");
+                    myScript.zonaEsferica.transform.localScale = new Vector3(myScript.range * 2, myScript.range * 2, myScript.range * 2);
+                    //myScript.zonaEsferica.GetComponent<Renderer>().material.shader = Shader.Find("Unlit/Transparent");
                     Color c = Color.yellow;
                     c.a = 0.3f;
-                    myScript.zonaEsfera.GetComponent<Renderer>().material.color = c;
-                    myScript.zonaEsfera.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+                    myScript.zonaEsferica.GetComponent<Renderer>().material.color = c;
+                    myScript.zonaEsferica.GetComponent<Renderer>().shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
                 }
                 if (myScript.range != myScript.oldRange)
                 {
                     myScript.oldRange = myScript.range;
-                    myScript.zonaEsfera.transform.position = myScript.objeto2.transform.position;
-                    myScript.zonaEsfera.transform.localScale = new Vector3(myScript.range * 2, myScript.range * 2, myScript.range * 2);
+                    myScript.zonaEsferica.transform.position = myScript.objeto2.transform.position;
+                    myScript.zonaEsferica.transform.localScale = new Vector3(myScript.range * 2, myScript.range * 2, myScript.range * 2);
                 }
             }
             else
             {
-                GameObject.DestroyImmediate(myScript.zonaEsfera);
-                myScript.zonaEsfera = null;
+                GameObject.DestroyImmediate(myScript.zonaEsferica);
+                myScript.zonaEsferica = null;
             }
         }
     }
